95e1821778fb860fc36ec80f9661fbd97417c91e
Dario <otrantodario@gmail.com>
2022-04-03 14:51:18 -0300

    Tile map progres

--- Assets/Scripts/Enemy Range.cs
+++ Assets/Scripts/Enemy Range.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class EnemyRange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    // Logic
+    public float triggerLenght = 1;
+    public float chaseLenght = 5;
+    private bool chasing;
+    private bool collidingWhitPlayer;
+    private Transform playerTransform;
+    private Vector3 startingPosition;
+
+    // Hitbox
+    public ContactFilter2D filter;
+    private BoxCollider2D hitBox;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected override void Start()
     {
-        
+        base.Start();
+        playerTransform = GameManeger.instance.player.transform;
+        startingPosition = transform.position;
+        hitBox = transform.GetChild(0).GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // Is the player in range?
+        if (Vector3.Distance(playerTransform.position, startingPosition) < chaseLenght)
+        {
+            if (Vector3.Distance(playerTransform.position, startingPosition) < triggerLenght)
+            {
+                chasing = true;
+            }
+            if (chasing)
+            {
+                if (!collidingWhitPlayer)
+                {
+                    UpdateMotor((playerTransform.position - transform.position).normalized);
+                }
+            }
+            else
+            {
+                UpdateMotor(startingPosition - transform.position);
+            }
+        }
+        else
+        {
+            UpdateMotor(startingPosition - transform.position);
+            chasing = false;
+        }
+
+        // Check for overlaps
+        collidingWhitPlayer = false;
+        // colition work
+        hitBox.OverlapCollider(filter, hits);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+            {
+                continue;
+            }
+
+            if (hits[i].tag == "Fighter" && hits[i].name == "Player")
+            {
+                collidingWhitPlayer = true;
+            }
+
+            // the array is not cleaned up , so we do it ourself
+            hits[i] = null;
+        }
+
+        UpdateMotor(Vector3.zero);
     }
+
 }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover 
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1.0f;
+
+    protected virtual void Start()
+    {
+        boxCollider = 
+    }
+    protected virtual void UpdateMotor(Vector3 imput)
+    {
+        // Reset the moveDelta
+        moveDelta = new Vector3(imput.x * xSpeed, imput.y * ySpeed, 0);
+    }
+}
