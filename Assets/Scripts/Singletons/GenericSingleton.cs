9c4809f0e39a25cae1a0adffb92ffca150ffb96d
Lucas Rodriguez Roge <lucas.rodriguez.roge@gmail.com>
2022-04-03 01:33:13 -0300

    Added scene transitioner.

---
+++ Assets/Scripts/Singletons/GenericSingleton.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Pathfinders.Toolbox.Singletons
+{
+    public class GenericSingleton<T> : MonoBehaviour where T : Component
+    {
+        #region EXPOSED_FIELDS
+        [SerializeField] protected bool dontDestroy = true;
+        #endregion
+
+        #region PRIVATE_FIELDS
+        private static T instance;
+        #endregion
+
+        #region PUBLIC_METHODS
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(T).Name;
+                        instance = obj.AddComponent<T>();
+                    }
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+        #region PROTECTED_METHODS
+        protected virtual void Awake()
+        {
+            Configure();
+        }
+        #endregion
+
+        #region PRIVATE_METHODS
+        private void Configure()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+
+                if (dontDestroy)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        #endregion
+    }
+}
+
